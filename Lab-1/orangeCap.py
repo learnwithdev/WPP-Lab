195731bb385ad1d576c637890f0d381d27f299ca
aiopswithdev <aiospwithdev@gmail.com>
2025-01-22 01:58:03 +0530

    reupload

--- Lab-1/orangeCap.py
+++ Lab-1/orangeCap.py
@@ -1,32 +1,23 @@
-from collections import defaultdict
-Test = [{'test1':{'Dhoni':74, 'Kohli': 150}}, {'test2': {'Dhoni':29, 'Kohli':143}}]
-res_dict = {}
+# Test = [{'test1':{'Dhoni':74, 'Kohli': 150}}, {'test2': {'Dhoni':29, 'Sachin':143}}]
+Test = [{'Dhoni':74, 'Kohli': 150}, {'Dhoni':29, 'Sachin':167}]
+
 dict1 = {}
 mylist = ["Dhoni", "Kohli", "Sachin"]
-
-# for i in range(0,2):
-#     mylist.append(Test[i])
-
 res_dict = {i: 0 for i in mylist}
 
 for i in range(0,2):
     dict1 = Test[i]
-    for k,v in dict1.items():
-        res_dict["Dhoni"] += v["Dhoni"]
-        res_dict["Kohli"] += v["Kohli"]
-        res_dict["Sachin"] += v["Sachin"]
-print(res_dict)
-
-# for i in range(0,2):
-#     dict1 = (Test[i].values())
-#     print(dict(dict1))
-    # l = list(dict1.keys())
-    # for s in l:
-    #     print(s)
-#         if key=='Dhoni':
-#             res_dict["Dhoni"] += value
-#         elif key=='Kohli':
-#             res_dict["Kohli"] += value
-#         elif key=='Sachin':
-#             res_dict["Sachin"] += value
+    if "Dhoni" in dict1:
+        res_dict["Dhoni"] += dict1["Dhoni"]
+    if "Kohli" in dict1:            
+        res_dict["Kohli"] += dict1["Kohli"]
+    if "Sachin" in dict1:
+        res_dict["Sachin"] += dict1["Sachin"]
 # print(res_dict)
+nmax = "Dhoni"
+rmax=res_dict[nmax]
+for k,v in res_dict.items():
+    if res_dict[k]>rmax:
+        rmax = res_dict[k]
+        nmax = k
+print(f"{nmax} wins the Orange Cap for {rmax} runs.")
